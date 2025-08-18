00b1db539307909ef7354468cf6450b615a990a5
jtlandis <jtlandis314@gmail.com>
2025-08-18 18:59:31 -0400

    Refactor ease constructor for flexible input and modes

--- R/util-ease.R
+++ R/util-ease.R
@@ -41,32 +41,86 @@ check_valid_ease <- function(f, name) {
 ease <- new_class(
   "ease",
   parent = class_function,
-  constructor = function(ease_in = ease_linear,
-                         ease_out = ease_in,
-                         ease_time = ease_linear) {
-    check_valid_ease(ease_in, "ease_in")
-    check_valid_ease(ease_out, "ease_out")
-    check_valid_ease(ease_time, "ease_time")
-    call_ease <- if (identical(ease_in, ease_out)) {
-      # Symmetric ease
-      ease_fn <- ease_in
-      quote(ease_fn(t))
-    } else {
-      # Asymmetric ease
-      call_ease_in <- quote(ease_in(t))
-      call_ease_out <- quote(ease_out(t))
-      substitute(((1 - t) * call_ease_in) + (t * call_ease_out))
+  constructor = function(ein = NULL,
+                         eout = NULL,
+                         etime = elinear,
+                         mode = c("default", "weighted")) {
+    ease_in <- ein
+    ease_out <- eout
+    ease_time <- etime
+    if (use_ease_in <- !is.null(ease_in)) {
+      check_valid_ease(ease_in, "ein")
+      ease_in <- substitute(ease_in(t))
     }
-    if (!identical(ease_time, identity)) {
-      call_ease <- substitute({
-        t <- ease_time(t)
+    if (use_ease_out <- !is.null(ease_out)) {
+      check_valid_ease(ease_out, "eout")
+      ease_out <- substitute((1 - ease_out(1 - t)))
+    }
+    check_valid_ease(ease_time, "etime")
+    res <- use_ease_in + use_ease_out
+    call_ease <- switch(res,
+      `1` = {
+        if (use_ease_in) {
+          ease_in
+        } else {
+          ease_out
+        }
+      },
+      `2` = {
+        mode <- match.arg(mode, c("default", "weighted"))
+        switch(mode,
+          default = {
+            ease_in <- ensub(ease_in, list(t = quote(t * 2)))
+            ease_in <- substitute(ease_in / 2)
+            ease_out <- ensub(ease_out, list(t = quote(((t - .5) * 2))))
+            ease_out <- substitute(0.5 + (ease_out / 2))
+            substitute({
+              if (t < .5) {
+                ease_in
+              } else {
+                ease_out
+              }
+            })
+          },
+          weighted = {
+            substitute(((1 - t) * ease_in) + (t * ease_out))
+          },
+        )
+      }
+    )
+
+    if (use_ease_time <- !identical(ease_time, identity)) {
+      ease_time <- substitute(t <- ease_time(t))
+    }
+    do <- sprintf(
+      "do%s%s",
+      if (use_ease_time) "time" else "",
+      if (!is.null(call_ease)) "value" else ""
+    )
+    func <- switch(do,
+      do = function(t) t,
+      dotime = substitute(function(t) {
+        ease_time
+        t
+      }) |> eval(),
+      dovalue = substitute(function(t) {
         call_ease
-      })
-    }
-    func <- function(t) {
-      t
-    }
-    body(func) <- call_ease
+      }) |> eval(),
+      dotimevalue = substitute(function(t) {
+        ease_time
+        call_ease
+      }) |> eval()
+    )
+    # func <- if (!is.null(call_ease)) {
+    # } else {
+    #   function(t) {
+    #     t
+    #   }
+    # }
+    # if (!is.null(call_ease)) {
+
+    # }
+    # body(func) <- call_ease
     new_object(func)
   }
 )
@@ -74,16 +128,17 @@ ease <- new_class(
 
 method(print, ease) <- function(x, ...) {
   cat("<ease> function (t) ")
-  print(
-    do.call(
-      substitute,
-      list(body(x), environment(x))
-    )
-  )
+  # print(
+  #   do.call(
+  #     substitute,
+  #     list(body(x), environment(x))
+  #   )
+  # )
+  print(body(x))
   invisible(x)
 }
 
 method(plot, ease) <- function(x, ...) {
   t <- seq(0, 1, length.out = 100)
-  plot(t, x(t), type = "l", xlab = "t", ylab = "ease(t)", main = "Ease Function")
+  plot(t, Vectorize(x)(t), type = "l", xlab = "t", ylab = "ease(t)", main = "Ease Function")
 }
